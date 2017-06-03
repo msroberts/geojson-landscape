79778b57119aaee72c11d7d6feff02db51701684
Madeleine Roberts <msroberts@users.noreply.github.com>
2017-06-03 09:54:37 -0400

    Set dependencies to external and add module name

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from 'rollup-plugin-typescript'
 import compiler from 'typescript'
 
+const {dependencies} = require('./package.json')
+
 const plugins = [
   typescript({
     // Use latest typescript
@@ -11,6 +13,8 @@ const plugins = [
 export default {
   entry: 'src/index.ts',
   dest: 'dist/bundle.js',
+  external: Object.keys(dependencies),
   format: 'umd',
+  moduleName: 'geojsonLandscape',
   plugins: plugins,
 }
