046578bd220dd8ddeda9eb7094d914f2780dbb1c
Madeleine Roberts <msroberts@users.noreply.github.com>
2017-06-04 07:31:06 -0400

    Support setting options

--- src/editor.ts
+++ src/editor.ts
@@ -3,7 +3,7 @@ import * as L from 'leaflet'
 export class Editor {
   public map: L.Map
 
-  constructor(public container: HTMLElement) {
-    this.map = L.map(container)
+  constructor(public container: HTMLElement, options: any = {}) {
+    this.map = L.map(container, options)
   }
 }
